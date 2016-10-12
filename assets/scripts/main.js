ac8f5e727ffe5ef51480440303383115d11fe5a2
Andres Munoz <mauricio2_3@hotmail.com>
2016-10-12 11:31:26 -0400

    Added the content and javascript

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,23 +1,117 @@
-(function(){
-  "use strict";
+//javascript onclick function
+$(document).ready(function(){
+    $(".learnmore").click(function(){
+        $(".learnmorecontent").slideToggle(500);
+    });
+});
 
-  var Author = function(name){
-    this.name = name || "Anonymous";
-    this.articles = [];
-  };
+//javascript Show and animate as you scroll function
 
-  Author.prototype.writeArticle = function(title){
-    this.articles.push(title);
-  };
+$(window).scroll(function() {
+    
+    $('.mission').each(function(){
 
-  Author.prototype.listArticles = function(){
-    return this.name + " has written: " + this.articles.join(", ");
-  };
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        
+        if (imagePos < topOfWindow+600) {
+            $(this).addClass("fadeRight");
+        }
+    });
 
-  exports.Author = Author;
+// WITH DELAY!
+    
+    $('.goal').each(function(){ 
+    
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        
+        if (imagePos < topOfWindow+600) {
+            setTimeout($.proxy(function(){
+                $(this).addClass("fadeLeft");
+            },this),200);
+        }
+    }); 
 
-  var peter = new Author("Peter");
-  peter.writeArticle("A Beginners Guide to npm");
-  peter.writeArticle("Using npm as a build tool");
-  peter.listArticles();
-})();
+});
+
+//On Click Scroll plus responsiveness
+
+
+
+//if(jQuery(window).width() < 568) {
+
+//          $('.parallaxcover .button').click(function(e)
+//               {
+//                  e.preventDefault();
+//                  $('html, body').animate({scrollTop: $('.gallery').offset().top-100}, 2500);
+//               });
+//}
+
+
+//else {
+$('.parallaxcover .button').click(function(e)
+ {
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('.contactus').offset().top-180}, 2500);
+ });
+//}
+
+
+//scroll java with responsiveness
+    
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 60) {
+        $(".logo").addClass("shrinklogo img");
+        
+        $(".navigation").css({
+            'background':'rgba(242, 242, 242, 0.4)',
+            'transition': '2s',
+            '-ms-transition': '2s',
+            '-moz-transition': '2s',
+            '-webkit-transition': '2s', 
+        });
+    
+    } 
+
+    else {
+        $(".logo").removeClass("shrinklogo img");
+        $(".navigation").css({
+                'background': 'rgba(23, 74, 115, 0.0)',
+                'transition': '.4s',
+                '-ms-transition': '.4s',
+                '-moz-transition': '.4s',
+                '-webkit-transition': '.4s',
+        });
+        
+    }
+    
+});
+
+
+if(jQuery(window).width() < 1200) {
+
+
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 200) {
+            $(".logo").addClass("shrinklogo img");
+            $(".navigation h2").css({
+                'color': '#28b8c5', 
+                'transition': '1s',
+                '-ms-transition': '1s',
+                '-moz-transition': '1s',
+                '-webkit-transition': '1s',
+
+            });
+        } 
+
+        else {
+            $(".logo").removeClass("shrinklogo img");
+        }
+
+    });
+}
